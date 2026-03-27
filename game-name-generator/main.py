be3fae2dd1701f011fc27df3702f2cdf6275830e
Ankit Maurya <95664001+anmaurya27@users.noreply.github.com>
2026-03-27 22:32:36 +0530

    Add files via upload

---
+++ game-name-generator/main.py
@@ -0,0 +1,134 @@
+
+#Project: Game Name Generator
+
+print("Hello! Fellow Gamers.\nWelcome To The Game Name Generator!!!")
+print("\nDo you guys wanna know a fun fact?")
+print("Game Names are called as 'ING' which means In-Game Name")
+input("press enter to continue_")
+
+print("\nSo please answer the following questions, this will help us find the perfect ING for you.")
+
+input("press enter_")
+
+print("\n-------------------------------------------------------------------------------------------------------")
+print("Question 1: Type a Title that you like."
+      + "\n(Or you can pick one from the following options: -\n"
+      + "\na. Royal Titles = King, Queen, Emperor, Empress, Prince, Lord, Monarch.\n"
+      + "\nb. Military Titles = Soldier, General, Lieutenant, Agent, Commander, Captain, Admiral.\n"
+      + "\nc. Class Titles = Wizard, Assasin, Mage, Ninja, Samurai, Warrior, Knight, Guardian, Archer, Berserker.)"
+      + " ")
+titles = input("\nYour answer = ")
+
+print("\n-------------------------------------------------------------------------------------------------------")
+print("Question 2: What is your fav Creature?"
+      + "\n(Or you can pick one from the following options: -\n"
+      + "\na. Natural Animals = Tiger, Lion, Panther, Wolf, Falcon, Viper, Bull, Rhino, etc.\n"
+      + "\nb. Mythical Beings = Dragon, Phoenix, Angel, Demon, Hydra, Griffin, Unicorn , Cerberus, Leviathan, Pegasus, etc."
+      + " ")
+creatures = input("\nYour answer = ")
+
+print("\n-------------------------------------------------------------------------------------------------------")
+print("Question 3: What is your fav Element?"
+      + "\n(Or you can pick one from the following options: -\n"
+      + "\na. Natural Elements = Fire, Wind, Air, Water, Earth, Ice, Lightning, Sand, Smoke, Magma, Metal, etc.\n"
+      + "\nb. Mythical Elements = Shadow, Light, Dark, Chaos, Time, Space, Spirit, Blood, Poison, Venom, Curse, etc."
+      + " ")
+elements = input("\nYour answer = ")
+
+print("\n-------------------------------------------------------------------------------------------------------")
+print("Question 4: What is your fav Texture?"
+      + "\n(Or you can pick one from the following options: -\n"
+      + "\na. Colours = Chrome, Crimson, Scarlet, Azure, Red, Green, Blue, Black, Grey, White, etc.\n"
+      + "\nb. Materials = Wood, Steel, Iron, Titanium, Obsidian, Gold, Silver, Glass, etc.\n"
+      + "\nc. Gemstones = Diamons, Emerald, Ruby, Sapphire, Amethyst, Moonstone, Aquamarine, etc."
+      + " ")
+textures = input("\nYour answer = ")
+
+print("\n-------------------------------------------------------------------------------------------------------")
+print("Question 5: What is your fav Weapon?"
+      + "\n(Or you can pick one from the following options: -\n"
+      + "\nWeapons = Blade, Hammer, Sword, Knife, Spike, Spear, Shield, Mace, Axe, Staff, Orb, etc."
+      + " ")
+weapons = input("\nYour answer = ")
+
+print("\n-------------------------------------------------------------------------------------------------------")
+print("Question 6: What is your fav Cosmic Forces?"
+      + "\n(Or you can pick one from the following options: -\n"
+      + "\nCosmic Forces = Star, Space, Nebula, Galaxy, Magnetar, Comet, Meteor, Supernova, Asteroid, Solar, etc."
+      + " ")
+cosmic = input("\nYour answer = ")
+
+print("\n-------------------------------------------------------------------------------------------------------")
+print("Question 7: You can type any word of your own, anything that you like to add."
+      + "\n(Or you can pick any words from the options in the previous questions: -"
+      + " ")
+own = input("\nYour answer = ")
+
+print("\n-------------------------------------------------------------------------------------------------------")
+print("These are your IGN: -\n")
+print("1. " + creatures + " " + titles)
+print("2. " + textures + " " + titles)
+print("3. " + elements + " " + titles)
+print("4. " + cosmic + " " + titles)
+print("5. " + weapons + " " + titles)
+print("6. " + own + " " + titles)
+
+print("7. " + textures + " " + creatures)
+print("8. " + elements + " " + creatures)
+print("9. " + cosmic + " " + creatures)
+print("10. " + weapons + " " + creatures)
+print("11. " + own + " " + creatures)
+
+print("12. " + textures + " " + elements)
+print("13. " + textures + " " + cosmic)
+print("14. " + textures + " " + weapons)
+print("15. " + textures + " " + own)
+
+print("16. " + elements + " " + cosmic)
+print("17. " + elements + " " + weapons)
+print("18. " + elements + " " + own)
+
+print("19. " + cosmic + " " + weapons)
+print("20. " + cosmic + " " + own)
+
+print("21. " + weapons + " " + own)
+
+print("\n-------------------------------------------------------------------------------------------------------")
+input("If you want to see the names in swapped order, \ne.g: Blue Dragon & Dragon Blue, \npress enter_")
+print("1. " + titles + " " + creatures)
+print("2. " + titles + " " + textures)
+print("3. " + titles + " " + elements)
+print("4. " + titles + " " + cosmic)
+print("5. " + titles + " " + weapons)
+print("6. " + titles + " " + own)
+
+print("7. " + creatures + " " + textures)
+print("8. " + creatures + " " + elements)
+print("9. " + creatures + " " + cosmic)
+print("10. " + creatures + " " + weapons)
+print("11. " + creatures + " " + own)
+
+print("12. " + elements + " " + textures)
+print("13. " + cosmic + " " + textures)
+print("14. " + weapons + " " + textures)
+print("15. " + own + " " + textures)
+
+print("16. " + cosmic + " " + elements)
+print("17. " + weapons + " " + elements)
+print("18. " + own + " " + elements)
+
+print("19. " + weapons + " " + cosmic)
+print("20. " + own + " " + cosmic)
+
+print("21. " + own + " " + weapons)
+
+
+
+# Python Concepts Used / Practised:
+# - print() function
+# - input() function
+# - Variables
+# - line break, \n
+# - sting concatenation, +
+
+
